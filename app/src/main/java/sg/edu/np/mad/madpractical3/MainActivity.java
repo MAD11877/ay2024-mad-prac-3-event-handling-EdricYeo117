44b52ccab0e39f3b0044ef61477397fe7286e6ce
EdricYeo117 <edricyeo.jr@gmail.com>
2024-04-27 22:49:43 +0800

    Intial Commit, ported over practical 2

---
+++ app/src/main/java/sg/edu/np/mad/madpractical3/MainActivity.java
@@ -0,0 +1,60 @@
+package sg.edu.np.mad.madpractical3;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    private Button buttonMessage;
+    private boolean followed;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        buttonMessage = findViewById(R.id.btnFollow);
+        buttonMessage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                followed = !followed;
+                updateButtonMessageText();
+            }
+        });
+
+        // Intialize a new User object
+        User user = new User("John Doe", "MAD Developer", 1, false);
+
+        // Get the TextViews and Button from Layout
+        TextView tvName = findViewById(R.id.tvName);
+        TextView tvDescription = findViewById(R.id.tvDescription);
+        Button btnFollow = findViewById(R.id.btnFollow);
+
+        // Set the TextViews with the User's name, description, deault button message
+        tvName.setText(user.name);
+        tvDescription.setText(user.description);
+        btnFollow.setText(getString(R.string.button_follow_text));
+
+        // Logic to update based on boolean
+
+    }
+    private void updateButtonMessageText() {
+        if (followed) {
+            buttonMessage.setText(R.string.button_unfollow_text);
+        } else {
+            buttonMessage.setText(R.string.button_follow_text);
+        }
+
+    }
+}
